c160d9165d26110669d2430801f504cded2b1bdb
Chris Underwood <cunderwoodmn@gmail.com>
2016-12-10 21:01:56 -0800

    add code for quiz to app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var questions = [
+  new Question("Who was the first President of the United States?", [" George Washington", "Thomas Jefferson"], "George Washington"),
+  new Question("Who is the MLB career leader in hits?", ["Ty Cobb", "Pete Rose"], "Pete Rose")
+];
+
+var quiz = new Quiz(questions);
